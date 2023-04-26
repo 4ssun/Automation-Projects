a8dd4c35a4ed60f50639a4075fd82ad609346252
Ana Assunção <anafatima02@gmail.com>
2023-04-26 16:10:03 -0300

    Update main.py

--- p01/main.py
+++ p01/main.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
 
-def webscraping():
+def Driver():
     driver = webdriver.Chrome(executable_path="D:\\Python\\chromedriver.exe")
     options = webdriver.ChromeOptions()
     options.arguments("disable-infobars")
     options.arguments("start-maximized")
     options.arguments("disable-dev-shm-usage")  #impede erros em máquinas linux!
     options.arguments("no-sandbox")  #para maiores privilégios de execução.
+
+def main():
+    driver = Driver()
+    element = driver.find_element(By. XPATH, "//*[@id="singlePageArticle"]/div[1]/div/div/div[2]/section/div/div/div/div/div/div/p[2]/span")
