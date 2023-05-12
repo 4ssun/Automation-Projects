70c8b76fcb41b99f4afc021e7b18c90d5604b562
Ana Assunção <anafatima02@gmail.com>
2023-05-12 15:35:02 -0300

    Update newsapi.py

--- others/newsapi.py
+++ others/newsapi.py
@@ -1,9 +1,19 @@
 # Biblioteca requests de HTTP com interação de API do site newsapi, retornando o conteúdo em modelo dict
 import requests
-r = requests.get(
-    'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=0275859d838f47cebe7fa143780f052b')
-conteudo = r.json()
-print(type(conteudo))  # dict
-conteudo = conteudo['articles']
-for artigo in conteudo:
-    print(artigo['title'])
+
+
+def get_artigo(topic, from_date, to_date, language='en', api_key='0275859d838f47cebe7fa143780f052b'):
+
+    url = f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}8&sortBy=popularity&language={language}&apiKey={api_key}'
+    r = requests.get(url)
+    conteudos = r.json()
+    print(conteudos)
+    artigo = conteudos['articles']
+    result = []
+    for busca in artigo:
+        result.append('Titulo:', busca['title'],  # type: ignore
+                      '\n Descrição:\n', busca['description'])
+        return result
+
+
+print(get_artigo(topic='space', from_date='2023-05-12', to_date='2023-04-11'))
